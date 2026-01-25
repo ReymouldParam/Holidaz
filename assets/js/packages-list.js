52f4f98bc68e09fea3555ca5cebcd71eb3c0d138
Revanth D <revanth.danduboina@reymould.com>
2026-01-25 23:24:04 +0530

    Packages page completed

---
+++ assets/js/packages-list.js
@@ -0,0 +1,34 @@
+const packages = [
+    {
+        id: 1,
+        imagePath: 'assets/images/carousel-image-1.webp',
+        countryName: 'Vietnam',
+        packageName: 'Package name goes here',
+        shortContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service.',
+        fullContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service. We work hard everyday, so that you could enjoy your holiday with our services. Since our establishment in 2014, we have sold more than 500 tours to various places in the world. Our guides, administrators and a lot of other employees who form our team understand what you need and try to make your tourist experience unforgettably outstanding.'
+    },
+    {
+        id: 2,
+        imagePath: 'assets/images/carousel-image-2.webp',
+        countryName: 'Thailan',
+        packageName: 'Package name goes here',
+        shortContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service. We work hard everyday, so that you could enjoy your holiday with our services',
+        fullContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service. We work hard everyday, so that you could enjoy your holiday with our services. Since our establishment in 2014, we have sold more than 500 tours to various places in the world. Our guides, administrators and a lot of other employees who form our team understand what you need and try to make your tourist experience unforgettably outstanding.'
+    },
+    {
+        id: 3,
+        imagePath: 'assets/images/carousel-image-3.webp',
+        countryName: 'Thailan',
+        packageName: 'Package name goes here',
+        shortContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service. We work hard everyday, so that you could enjoy your holiday with our services',
+        fullContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service. We work hard everyday, so that you could enjoy your holiday with our services. Since our establishment in 2014, we have sold more than 500 tours to various places in the world. Our guides, administrators and a lot of other employees who form our team understand what you need and try to make your tourist experience unforgettably outstanding.'
+    },
+    {
+        id: 4,
+        imagePath: 'assets/images/carousel-image-4.webp',
+        countryName: 'Thailan',
+        packageName: 'Package name goes here',
+        shortContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service. We work hard everyday, so that you could enjoy your holiday with our services',
+        fullContent: 'Holidaz was founded 3 years ago as an ultimate place for Indian residents to receive high quality travelling service. We work hard everyday, so that you could enjoy your holiday with our services. Since our establishment in 2014, we have sold more than 500 tours to various places in the world. Our guides, administrators and a lot of other employees who form our team understand what you need and try to make your tourist experience unforgettably outstanding.'
+    },
+]
---
+++ assets/js/packages-page.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+
+    /* -----------------------------
+       Initial State
+    ----------------------------- */
+    $('#packageDetailsContainer').hide();
+    $('#packageCarouselContainer').show();
+
+    /* -----------------------------
+       Helper Methods
+    ----------------------------- */
+    function getPackageById(id) {
+        return packages.find(pkg => pkg.id === id);
+    }
+
+    function getImage(id) {
+        return getPackageById(id)?.imagePath || '';
+    }
+
+    function getShortContent(id) {
+        return getPackageById(id)?.shortContent || '';
+    }
+
+    function getFullContent(id) {
+        return getPackageById(id)?.fullContent || '';
+    }
+
+    function getPackageName(id) {
+        return getPackageById(id)?.packageName || '';
+    }
+
+    function getCountryName(id) {
+        return getPackageById(id)?.countryName || '';
+    }
+
+    /* -----------------------------
+       Render Carousel
+    ----------------------------- */
+    function renderCarousel() {
+        const $carousel = $('.packages-carousel-container');
+        $carousel.empty();
+
+        packages.forEach(pkg => {
+            const item = `
+                <div class="item" data-id="${pkg.id}">
+                    <div class="inner-container">
+                        <div class="image-container" style="background-image:url(${pkg.imagePath})">
+                            <div class="details">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <p class="title">${pkg.packageName}</p>
+                                    <button class="view-more-btn" data-id="${pkg.id}">
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </button>
+                                </div>
+                                <p class="data mb-0">${pkg.shortContent}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="country-name">${pkg.countryName}</p>
+                </div>
+            `;
+            $carousel.append(item);
+        });
+
+        // Initialize slick AFTER rendering
+        $carousel.slick({
+            dots: false,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            arrows: true,
+            centerMode: true,
+            centerPadding: '0',
+            nextArrow: '<button class="nextBtn slick-btn"><img src="assets/images/airplane-icon-right.png"></button>',
+            prevArrow: '<button class="prevBtn slick-btn"><img src="assets/images/airplane-icon-left.png"></button>',
+        });
+    }
+
+    renderCarousel();
+
+    /* -----------------------------
+       View More Click
+    ----------------------------- */
+    $(document).on('click', '.view-more-btn', function () {
+        const id = Number($(this).data('id'));
+
+        // Pause carousel autoplay
+        $('.packages-carousel-container').slick('slickPause');
+
+        $('#packageCarouselContainer').hide();
+        $('#packageDetailsContainer').show();
+
+        $('#packageDetailsContainer .image')
+            .css('background-image', `url(${getImage(id)})`);
+
+        $('#packageDetailsContainer .title')
+            .text(getPackageName(id));
+
+        $('#packageDetailsContainer .data')
+            .text(getFullContent(id));
+    });
+
+    /* -----------------------------
+       Close Button
+    ----------------------------- */
+    $('.close-btn').on('click', function () {
+        $('#packageDetailsContainer').hide();
+        $('#packageCarouselContainer').show();
+
+        // Resume carousel autoplay
+        $('.packages-carousel-container').slick('slickPlay');
+    });
+
+});
